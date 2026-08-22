9636a66e0b4b9565b8ac10692a7bbd5c691da9d1
YunHair <71691550+YunHair@users.noreply.github.com>
2026-08-22 14:29:23 +0800

    Update custom_feed.sh
    Signed-off-by: YunHair <71691550+YunHair@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -195,7 +195,7 @@ install_custom_feed() {
         "sbwml/luci-app-mosdns|https://github.com/sbwml/luci-app-mosdns.git|v5|mosdns luci-app-mosdns"
         "Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
         "nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
-        "yunhair-package|https://github.com/YunHair/openwrt-package.git||gecoosac luci-app-gecoosac luci-app-wolultra usteer-ng luci-app-advancedplus  " 
+        "yunhair-package|https://github.com/YunHair/openwrt-package.git||gecoosac luci-app-gecoosac luci-app-wolultra usteer-ng luci-app-advancedplus luci-app-usteer " 
     )
 
     feeds_path=$(get_feeds_path)
