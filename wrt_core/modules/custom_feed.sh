1e17b86859736c2f93b79449e73d1f94c912a647
YunHair <71691550+YunHair@users.noreply.github.com>
2026-08-27 22:14:33 +0800

    Update custom_feed.sh
    Signed-off-by: YunHair <71691550+YunHair@users.noreply.github.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -193,8 +193,8 @@ install_custom_feed() {
     custom_feed_sources=(
         "kenzok8/small-package|https://github.com/kenzok8/small-package.git||${base_custom_feed_packages[*]}"
         "sbwml/luci-app-mosdns|https://github.com/sbwml/luci-app-mosdns.git|v5|mosdns luci-app-mosdns"
-        #"Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
-        #"nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
+        "Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
+        "nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
         "yunhair-package|https://github.com/YunHair/openwrt-package.git||gecoosac luci-app-gecoosac luci-app-advancedplus lucky luci-app-lucky luci-app-zerotier" 
     )
 
